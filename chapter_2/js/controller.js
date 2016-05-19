c8e131d0bedba1dc4cdbac0a7855c6b8253e3319
marcin.matula <marqnpl@gmail.com>
2016-05-19 15:46:23 +0200

    factory added

--- chapter_2/js/controller.js
+++ chapter_2/js/controller.js
@@ -1,41 +1,66 @@
 var app = angular.module('demo', ['ngRoute']);
 
+app.factory('demoService', function demoServiceFactory() {
+    return '123cba!!!';
+});
 
+app.factory('anotherService', function anotherServiceFactory(demoService) {
+    return demoService;
+});
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider
-      .when('/',{
-          templateUrl:"pages/contact.html"
-      })
-      .when('/index',{
-          templateUrl: 'pages/index.html'
-      })
-      .when('/test',{
-          template: '<h1>Test</h1><a href="#/">Back</a>'
-      })
-      .when('/add-contact',{
-          templateUrl: 'pages/add-contact.html'
-      })
-      .otherwise({
-        template: '<h1>Not Found</h1>'
-      });
+        .when('/', {
+            templateUrl: "pages/contact.html"
+        })
+        .when('/index', {
+            controller: 'indexCtrl',
+            templateUrl: 'pages/index.html'
+        })
+        .when('/test', {
+            template: '<h1>Test</h1><a href="#/">Back</a>'
+        })
+        .when('/add-contact', {
+            templateUrl: 'pages/add-contact.html'
+        })
+        .otherwise({
+            template: '<h1>Not Found</h1>'
+        });
 });
 
 
+app.controller('indexCtrl', function ($scope, anotherService) {
 
-app.controller('indexCtrl', function ($scope) {
-  $scope.test = 'indexCtrl corp.';
+    $scope.demo = anotherService;
+    
+    $scope.contacts = [
+        {
+            name: 'Stefan Autorski',
+            phone: '01234543210',
+            address: 'al. Inna 12\nKrzyżówkowo\n11-111',
+            email: 'steve228uk@gmail.com',
+            website: 'stefanautorski.to',
+            notes: ''
+        },
+        {
+            name: 'Janko Walski',
+            phone: '0123456789',
+            address: "ul. Zagadkowa 123\nSzarada Duża\n10-010 Polska",
+            email: 'janko@walski.com',
+            website: 'http://janko-walski.info',
+            notes: 'Kilka słów na temat Janko.'
+        }
+    ];
 });
 
 app.controller('addCtrl', function ($scope) {
-  $scope.test = 'addCtrl corp.';
+    $scope.test = 'addCtrl corp.';
 });
 
-app.controller('contactCtrl', function($scope) {
-  $scope.test = 'contactCtrl corp.';
+app.controller('contactCtrl', function ($scope) {
+    $scope.test = 'contactCtrl corp.';
 });
 
-app.controller('AppCtrl' , function($scope)
-{
-  $scope.test = 'mqn corps.';
+app.controller('AppCtrl', function ($scope) {
+    $scope.test = 'mqn corps.';
 });
